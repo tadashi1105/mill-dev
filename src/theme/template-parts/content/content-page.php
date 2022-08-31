b5d83fca16bf8dbc2a86ced395d4452a819c9679
Tadashi Matsuura <4456384+tadashi1105@users.noreply.github.com>
2022-08-31 15:00:25 +0000

    fix: fixed missing double quotes for closing class attributes

--- src/theme/template-parts/content/content-page.php
+++ src/theme/template-parts/content/content-page.php
@@ -8,7 +8,7 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( [ 'p-2', 'p-sm-6', 'shadow', 'bg-surface-primary', 'rounded-4' ] ); ?>>
 	<header class="mb-4">
-		<?php the_title( '<h1 class="h2 text-break>', '</h1>' ); ?>
+		<?php the_title( '<h1 class="h2 text-break">', '</h1>' ); ?>
 	</header>
 	<div class="mb-4">
 		<?php get_template_part( 'template-parts/post-thumbnail' ); ?>
